603c4ea09c9abb8ba58c21afed7ee4b90cb7aa3d
Dan Octavian <dan@danpad.(none)>
2013-06-22 12:22:59 -0700

    added mongoscripts wrote some shit in tresh finder

--- jwikimatch/src/com/amazon/ml/ThresholdFinder.scala
+++ jwikimatch/src/com/amazon/ml/ThresholdFinder.scala
@@ -1,12 +1,41 @@
 package com.amazon.ml
 
-// cls = classficiation
-class TreshCase(id : Int, tresh: Double, cls: Int) {
+import com.amazon.ml.TreshCase
+import scala.collection.JavaConversions
+
+/* bucket sort based on doubles with values in range 0 - 1 */
+class BucketList(decs : Int) {
+
+  var bucks : Array[List[HasDouble]] = Array()
+  var mult : Int = 1
+  /* decs -number of decimals therefore of buckets */
+  def create(cases : List[HasDouble])  = {
+    var d :Int = Math.min(5, decs)
+    mult = Math.pow(10.0, decs.toDouble).toInt
+    bucks = new Array(mult)
+		bucks.map(_ => List())
+  }
+
+  /* PRE: the double is in the 0 -1 range */
+  def insert(e : HasDouble) = {
+//		bucks[e.getDouble * mult].
+  }
+
+  def foreach[U](f: HasDouble => U): Unit = {
+    bucks.foreach(list => { list.foreach(f)})
+  }  
 }
+
+trait HasDouble {
+  	def getDouble() : Double
+}
+
 
 object ThresholdFinder {
 	type Tresh = Double
-	def find(): (Tresh, Tresh) = {
-			return (1, 1)
+	def find(cases : List[TreshCase], decs: Int): (Tresh, Tresh) = {
+			
+			(1, 1)
 	}
+	
 }
